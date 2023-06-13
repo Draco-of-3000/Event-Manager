1669ca4a6d0a85a56bc478d38757998092df1e44
Draco <kevin.tenisson@gmail.com>
2023-06-13 04:26:45 -0700

    Cleaned up phone numbers

--- event_manager.rb
+++ event_manager.rb
@@ -7,6 +7,22 @@ def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_number(number)
+  number.gsub!(/[\s\(\)\-\.]/, '')
+
+  if number.length < 10
+    number = 'Invalid number'
+  elsif number.length == 11 && number[0] == '1'
+    number = number[1..-1]
+  elsif number.length == 1 && number[0] != 1
+    number = 'Invalid number'
+  elsif number.length > 11
+    number = 'Invalid number'
+  else
+    number
+  end
+end
+
 def legislators_by_zipcode(zip)
     civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
     civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -51,13 +67,18 @@ contents.each do |row|
     id = row[0]
     name = row[:first_name]
     zipcode = row[:zipcode]
+    number = row[:homephone]
 
     zipcode = clean_zipcode(zipcode)
+
+    number = clean_number(number)
 
     legislators = legislators_by_zipcode(zipcode)
 
     form_letter = erb_template.result(binding)
 
     save_thank_you_letter(id,form_letter)
+
+    puts "#{name} #{zipcode} #{legislators} #{number}"
 end
 
